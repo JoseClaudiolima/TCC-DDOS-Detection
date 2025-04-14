1eef94b7c4d20fd84e27e8b88a334397fd1a6709
JoseClaudioLima <jose.c.lima.sp@gmail.com>
2025-04-14 13:22:17 -0300

    Updated the gitignore

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
 
     # Interação com banco de dados:
     try:
+        # Exemplos dos comandos no banco de dados:
         # insert_malicious_ip(connection, cursor, "192.168.0.1", "::1")
         # select_all_malicious_ips(cursor)
         # delete_all_ip(connection, cursor)
